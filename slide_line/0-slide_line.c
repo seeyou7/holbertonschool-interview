881996a18c0ceb3e7af5fea79049d7a31be04472
seeyou7 <5764@holbertonstudents.com>
2024-11-20 10:59:21 +0100

    fix betty style

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,110 +1,103 @@
 #include "slide_line.h"
-
+#include <stddef.h>
 /**
-* slide_left - Slides array to left
-*
-* @line: Points to an array of integers containing size elements
-*
-* @size: Size of the array
-*
-* Return: Void
-*/
-
-void slide_left(int *line, size_t size)
-{
-	size_t index, position = 0, temp;
-
-	for (index = 0; index < size && position; index++)
-	{
-		while (line[position] == 0 && (position < size) && (position + 1 < size))
-		{
-			position++;
-		}
-		if (!line[index])
-		{
-			temp = line[position];
-			line[position] = line[index];
-			line[index] = temp;
-		}
-		position++;
-	}
-}
-
-/**
-* slide_right - Slides array to right
-*
-* @line: Points to an array of integers containing size elements
-*
-* @size: Size of the array
-*
-* Return: Void
-*/
-
-void slide_right(int *line, size_t size)
-{
-	size_t index, temp, position;
-
-	position = size - 1;
-
-	for (index = size - 1; (int)index >= 0 && (int)position >= 0; index--)
-	{
-		while (line[position] == 0 && position > 0)
-		{
-			position--;
-		}
-		if (!line[index])
-		{
-			temp = line[position];
-			line[position] = line[index];
-			line[index] = temp;
-		}
-		position--;
-	}
-}
-
-/**
-* slide_line - Reproduces 2048 on one line
-*
-* @line: Points to an array of integers containing size elements
-*
-* @size: Size of the array
-*
-* @direction: Direction to slide
-*
-* Return: 1 on success, 0 on failure
-*/
-
+ * slide_line - Slide and merge an array of integers in the given direction.
+ *
+ * @line: Pointer to the array of integers.
+ * @size: Number of elements in the array.
+ * @direction: SLIDE_LEFT or SLIDE_RIGHT.
+ *
+ * Return: 1 upon success, 0 upon failure.
+ **/
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t index;
+    size_t i, j;
 
-	if (direction == SLIDE_LEFT)
-	{
-		slide_left(line, size);
-		for (index = 0; index < size; index++)
-		{
-			if (line[index] == line[index + 1])
-			{
-				line[index] = line[index] + line[index + 1];
-				line[index + 1] = 0;
-			}
-		}
-		slide_left(line, size);
-		return (1);
-	} else if (direction == SLIDE_RIGHT)
-	{
-		slide_right(line, size);
+    if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
+        return 0;
 
-		for (index = size - 1; (int)index >= 0; index--)
-		{
-			if (line[index] == line[index - 1])
-			{
-				line[index] = line[index] + line[index - 1];
-				line[index - 1] = 0;
-			}
-		}
-		slide_right(line, size);
-		return (1);
-	}
-	return (0);
+    if (direction == SLIDE_LEFT)
+    {
+        for (i = 0, j = 0; i < size; i++)
+        {
+            if (line[i] != 0)
+            {
+                if (j != i)
+                {
+                    line[j] = line[i];
+                    line[i] = 0;
+                }
+                j++;
+            }
+        }
+
+        for (i = 0; i < size - 1; i++)
+        {
+            if (line[i] == line[i + 1])
+            {
+                line[i] *= 2;
+                line[i + 1] = 0;
+            }
+        }
+
+        for (i = 0, j = 0; i < size; i++)
+        {
+            if (line[i] != 0)
+            {
+                if (j != i)
+                {
+                    line[j] = line[i];
+                    line[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+    else
+    {
+        for (i = size - 1, j = size - 1; j < size; i--)
+        {
+            if (line[i] != 0)
+            {
+                if (j != i)
+                {
+                    line[j] = line[i];
+                    line[i] = 0;
+                }
+                j--;
+            }
+            if (i == 0)
+            {
+                break;
+            }
+        }
+
+        for (i = size - 1; i > 0; i--)
+        {
+            if (line[i] == line[i - 1])
+            {
+                line[i] *= 2;
+                line[i - 1] = 0;
+            }
+        }
+
+        for (i = size - 1, j = size - 1; j < size; i--)
+        {
+            if (line[i] != 0)
+            {
+                if (j != i)
+                {
+                    line[j] = line[i];
+                    line[i] = 0;
+                }
+                j--;
+            }
+            if (i == 0)
+            {
+                break;
+            }
+        }
+    }
+
+    return 1;
 }
